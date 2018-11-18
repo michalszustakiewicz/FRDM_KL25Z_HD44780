e4c8a6db7e9a9b16706fcd6db9ab53083ad5ee2b
Michał Szustakiewicz <40272497+michalszustakiewicz@users.noreply.github.com>
2018-11-18 16:26:32 +0100

    Add files via upload

---
+++ Events.h
@@ -0,0 +1,94 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : kl25z_HD44780
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2018-05-27, 14:25, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "LCD1.h"
+#include "EN1.h"
+#include "BitIoLdd1.h"
+#include "RS1.h"
+#include "BitIoLdd2.h"
+#include "DB41.h"
+#include "BitIoLdd7.h"
+#include "DB51.h"
+#include "BitIoLdd8.h"
+#include "DB61.h"
+#include "BitIoLdd9.h"
+#include "DB71.h"
+#include "BitIoLdd10.h"
+#include "WAIT1.h"
+#include "MCUC1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd11.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL25Z128LK4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void);
+
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
